ebcace3a234c08e8fb10c06d987cd1b7029ebfc1
makena <roselynemakena@gmail.com>
2017-03-03 15:03:23 +0300

    Add carousel

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
-  # skip_before_action :authenticate_user!
   skip_before_filter :verify_authenticity_token
- 
+
+  skip_before_action :authenticate_user!
+
   def index
   if signed_in?
     if current_user.is_admin?(current_user)
@@ -11,14 +12,15 @@ class HomeController < ApplicationController
     elsif current_user.is_donor?(current_user)
       #redirect to donor root
       redirect_to donor_dashboard_path
-    end
 
     elsif current_user.is_recepient?(current_user)
-      #redirect to donor root
+      #redirect to rec root
       redirect_to recepient_dashboard_path
     end
 
   end
+end
+
 
 
 end
