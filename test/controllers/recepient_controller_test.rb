c7159e094f3b5f6267b1a5d33cfb9871b935a1b5
makena <roselynemakena@gmail.com>
2017-03-14 10:28:11 +0300

    Cleanup

--- test/controllers/recepient_controller_test.rb
+++ test/controllers/recepient_controller_test.rb
@@ -1,9 +1,9 @@
-require 'test_helper'
+# require 'test_helper'
 
-class RecepientControllerTest < ActionDispatch::IntegrationTest
-  test "should get dashboard" do
-    get recepient_dashboard_url
-    assert_response :success
-  end
+# class RecepientControllerTest < ActionDispatch::IntegrationTest
+#   test "should get dashboard" do
+#     get recepient_dashboard_url
+#     assert_response :success
+#   end
 
-end
+# end
