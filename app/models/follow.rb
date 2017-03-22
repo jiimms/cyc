22126838ea678913d52dfc31e875bbaa04d17da4
makena <roselynemakena@gmail.com>
2017-03-22 11:55:57 +0300

    Correct user follows

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -5,10 +5,11 @@ belongs_to :user
 belongs_to :follower, :class_name => 'User'
 
 
-	# def already_following?(user_id, follower_id)
-	# 	if Follow.find(:follower, conditions: ["recepient_id = ? and user_id = ?", self.recepient_id, self.user_id]).any?
-	# 		raise "Error"
-	# 	end
+	def self.already_following?(user_id, follower_id)
+		if Follow.where(user_id: user_id, follower_id: follower_id).any?
+			return true
+		end
+
 		
-	# end
+	end
 end
